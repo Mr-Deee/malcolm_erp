23a3e90a57aa445bd6d19e165e3ac74bcef59ee5
MR.NARTERH <merchantdaniel8@gmail.com>
2024-02-21 18:18:03 +0000

    sold

---
+++ lib/pages/Sold.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Soldpage extends StatefulWidget {
+  const Soldpage({super.key});
+
+  @override
+  State<Soldpage> createState() => _SoldpageState();
+}
+
+class _SoldpageState extends State<Soldpage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
