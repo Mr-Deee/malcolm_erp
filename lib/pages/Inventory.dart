43f1c0042bf8e88b3dec9b9a4dd7cd57cc7d66d0
Merchant_Daniel <merchantdaniel8@gmail.com>
2024-02-26 16:57:37 +0000

    inventory details

--- lib/pages/Inventory.dart
+++ lib/pages/Inventory.dart
@@ -18,31 +18,39 @@ class _InventoryState extends State<Inventory> {
       body: Container(
         padding: EdgeInsets.all(10.0),
         child: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('utils').doc('ProductCategory').snapshots(),
+          stream: FirebaseFirestore.instance
+              .collection('utils')
+              .doc('ProductCategory')
+              .snapshots(),
           builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return CircularProgressIndicator(); // Display a progress indicator while fetching data
             }
             if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
+              return Text('Error:${snapshot.error}');
             }
             if (snapshot.hasData && snapshot.data!.exists) {
-              Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-              List<dynamic> categories = data['list']; // Assuming category names are stored as a list
+              Map<String, dynamic> data =
+                  snapshot.data!.data() as Map<String, dynamic>;
+              List<dynamic> categories =
+                  data['list']; // Assuming category names are stored as a list
               return ListView.builder(
                 itemCount: categories.length,
                 itemBuilder: (context, index) {
                   String categoryName = categories[index];
                   return GestureDetector(
                     onTap: () {
-                      // Navigate or show inventory based on the category name
-                      // You can implement this part based on your navigation requirements
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Inventorydetails()));
                       print('Tapped on category: $categoryName');
                     },
                     child: Card(
                       child: ListTile(
                         title: Text(categoryName),
-                        leading: Icon(Icons.category), // Placeholder icon, replace with actual icons
+                        leading: Icon(Icons
+                            .category), // Placeholder icon, replace with actual icons
                       ),
                     ),
                   );
---
+++ lib/pages/Inventorydetails.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Inventorydetails extends StatefulWidget {
+  const InventInventorydetailsory({Key? key}) : super(key: key);
+
+  @override
+  State<Inventorydetails> createState() => _InventorydetailsState();
+}
+
+class _InventorydetailsState extends State<Inventorydetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Inventory Details"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: 
+        StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('utils')
+              .doc('ProductCategory')
+              .snapshots(),
+          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator(); // Display a progress indicator while fetching data
+            }
+            if (snapshot.hasError) {
+              return Text('Error:${snapshot.error}');
+            }
+            if (snapshot.hasData && snapshot.data!.exists) {
+              Map<String, dynamic> data =
+                  snapshot.data!.data() as Map<String, dynamic>;
+              List<dynamic> categories =
+                  data['list']; // Assuming category names are stored as a list
+              return ListView.builder(
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  String categoryName = categories[index];
+                  return GestureDetector(
+                    onTap: () {
+                      print('Tapped on category: $categoryName');
+                    },
+                    child: Card(
+                      child: ListTile(
+                        title: Text(categoryName),
+                        leading: Icon(Icons
+                            .category), // Placeholder icon, replace with actual icons
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+            return Text('No categories found');
+          },
+        ),
+      ),
+    );
+  }
+}
