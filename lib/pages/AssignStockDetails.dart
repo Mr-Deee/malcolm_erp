24fd9c6b8b6dab1c5f3d27c96dfb05a9ab8c8284
MR.NARTERH <merchantdaniel8@gmail.com>
2024-03-17 18:45:08 +0000

    Assigned update

--- lib/pages/AssignStockDetails.dart
+++ lib/pages/AssignStockDetails.dart
@@ -10,6 +10,7 @@ class _AssignStockPageState extends State<AssignStockPage> {
   String? _selectedUser;
   String? _selectedProduct;
   String? _selectedProductid;
+
   // String? Email;
   double _totalPrice = 0.0;
   double? _costPrice;
@@ -18,7 +19,10 @@ class _AssignStockPageState extends State<AssignStockPage> {
   String? _selectedUserEmail;
 
   Future<void> fetchUserEmail(value) async {
-    final querySnapshot = await FirebaseFirestore.instance.collection('users').where('FullName', isEqualTo: value).get();
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .where('FullName', isEqualTo: value)
+        .get();
     if (querySnapshot.docs.isNotEmpty) {
       setState(() {
         _selectedUserEmail = querySnapshot.docs.first['Email'] as String?;
@@ -81,9 +85,10 @@ class _AssignStockPageState extends State<AssignStockPage> {
                   final products = snapshot.data?.docs
                       .map((doc) => doc['Product'] as String)
                       .toList();
-                  final productIds = snapshot.data?.docs.map((doc) => doc.id).toList(); // Added
+                  final productIds = snapshot.data?.docs
+                      .map((doc) => doc.id)
+                      .toList(); // Added
                   final productMap = Map.fromIterables(products!, productIds!);
-
 
                   return Column(
                     children: [
@@ -96,7 +101,8 @@ class _AssignStockPageState extends State<AssignStockPage> {
                           print(
                               "Selected Product ID: ${productMap[value]}"); // Added
                         },
-                        items: products.map<DropdownMenuItem<String>>((String product) {
+                        items: products
+                            .map<DropdownMenuItem<String>>((String product) {
                           return DropdownMenuItem<String>(
                             value: product,
                             child: Text(product),
@@ -143,9 +149,9 @@ class _AssignStockPageState extends State<AssignStockPage> {
                               double price = doc.data()?['Cost'];
                               setState(() {
                                 _totalPrice = quantity * price;
-                              _costPrice = price ;
+                                _costPrice = price;
 
-                                _selectedProductid=productId;
+                                _selectedProductid = productId;
                                 print("price" '$quantity');
                                 print("prod: " '$productId');
                                 // print("int"'$price');
@@ -183,29 +189,95 @@ class _AssignStockPageState extends State<AssignStockPage> {
                         actions: [
                           ElevatedButton(
                             onPressed: () {
+                              // Logic to assign stock
 
                               // Logic to assign stock
-                              final int soldQuantity = int.tryParse(_quantityController.text) ?? 0;
-                              FirebaseFirestore.instance.collection('AssignedStock').add({
-                                'quantity':soldQuantity,
-                                'Costper':_costPrice,
-                                'total': _totalPrice,
-                                'ProductName': _selectedProduct,
-                                'soldQuantity': 0,
-                                'totalSales': 0.0,
-                                'ProductID':_selectedProductid,
-                                'User': _selectedUser,
-                                'Email': _selectedUserEmail?.toLowerCase().toString(),
-                              }).then((value) {
-                                Navigator.of(context).pop();
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text('Data written successfully')),
+                              final int soldQuantity =
+                                  int.tryParse(_quantityController.text) ?? 0;
+                              final double costPrice = _costPrice ?? 0.0;
+                              final double totalPrice = _totalPrice;
+                              final String productName = _selectedProduct ?? '';
+                              final String productId = _selectedProductid ?? '';
+                              final String userEmail = _selectedUserEmail
+                                      ?.toLowerCase()
+                                      .toString() ??
+                                  '';
 
-                                );
+                              // Check if the product already exists in AssignedStock
+                              FirebaseFirestore.instance
+                                  .collection('AssignedStock')
+                                  .where('ProductID', isEqualTo: productId)
+                                  .where('User', isEqualTo: _selectedUser)
+                                  .get()
+                                  .then((QuerySnapshot querySnapshot) {
+                                if (querySnapshot.docs.isNotEmpty) {
+                                  // Product already exists, update quantity and total
+                                  final doc = querySnapshot.docs.first;
+                                  final int existingQuantity = doc['quantity'];
+                                  final double existingTotal = doc['total'];
+                                  final int newQuantity =
+                                      existingQuantity + soldQuantity;
+                                  final double newTotal =
+                                      existingTotal + totalPrice;
+                                  doc.reference.update({
+                                    'quantity': newQuantity,
+                                    'total': newTotal,
+                                  }).then((value) {
+                                    Navigator.of(context).pop();
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                          content: Text(
+                                              'Stock updated successfully')),
+                                    );
+                                  }).catchError((error) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                          content: Text(
+                                              'Failed to update stock: $error')),
+                                    );
+                                    Navigator.of(context).pop();
+                                  });
+                                } else {
+                                  final int soldQuantity =
+                                      int.tryParse(_quantityController.text) ??
+                                          0;
+                                  FirebaseFirestore.instance
+                                      .collection('AssignedStock')
+                                      .add({
+                                    'quantity': soldQuantity,
+                                    'Costper': _costPrice,
+                                    'total': _totalPrice,
+                                    'ProductName': _selectedProduct,
+                                    'soldQuantity': 0,
+                                    'totalSales': 0.0,
+                                    'ProductID': _selectedProductid,
+                                    'User': _selectedUser,
+                                    'Email': _selectedUserEmail
+                                        ?.toLowerCase()
+                                        .toString(),
+                                  }).then((value) {
+                                    Navigator.of(context).pop();
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                          content: Text(
+                                              'Data written successfully')),
+                                    );
+                                  }).catchError((error) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                          content: Text(
+                                              'Failed to write data: $error')),
+                                    );
+                                    Navigator.of(context).pop();
+                                  });
+                                }
                               }).catchError((error) {
                                 ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text('Failed to write data: $error')),
-                                ); Navigator.of(context).pop();
+                                  SnackBar(
+                                      content: Text(
+                                          'Error checking existing stock: $error')),
+                                );
+                                Navigator.of(context).pop();
                               });
                             },
                             child: Text('Assign'),
@@ -229,7 +301,4 @@ class _AssignStockPageState extends State<AssignStockPage> {
       ),
     );
   }
-
-
-
 }
